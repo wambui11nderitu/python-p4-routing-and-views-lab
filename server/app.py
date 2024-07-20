7ec4350b6eb02fbc7c9c5630fe01465f42622be1
wambui11nderitu <wambuinderitu93@gmail.com>
2024-07-20 22:16:18 +0300

    updating labs

--- server/app.py
+++ server/app.py
@@ -4,5 +4,47 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:number>')
+def count(number):
+    count = f''
+    for n in range(number):
+        count += f'{n}\n'
+    return count
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        if num2 != 0:
+            return str(num1 / num2)
+        else:
+            return "Cannot divide by zero."
+    elif operation == '%':
+        if num2 != 0:
+            return str(num1 % num2)
+        else:
+            return "Cannot modulo by zero."
+    else:
+        return "Invalid operation."
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+
+
+
+
